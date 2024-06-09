d7851d8982067e78ec724239fe2b20446d394a53
mohsen <mkhashei@dev.ciu.edu.tr>
2024-06-09 10:49:36 +0300

    some update

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -5,8 +5,180 @@
 <link href="https://fonts.googleapis.com/css?family=Krub&display=swap" rel="stylesheet" />
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet" />
-<link href="{{asset('css/main.css')}}" rel="stylesheet" />
 <style>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px 50px;
+        background-color: #191A32;
+    }
+
+    header .logo {
+        display: flex;
+        align-items: center;
+    }
+
+    header .logo img {
+        height: 40px;
+        margin-right: 10px;
+    }
+
+    header nav ul {
+        list-style: none;
+        display: flex;
+        margin: 0;
+        padding: 0;
+    }
+
+    header nav ul li {
+        margin-right: 20px;
+
+    }
+
+    header nav ul li a {
+        text-decoration: none;
+        color: #FFFFFF;
+        font-size: 14px;
+    }
+
+    header nav ul li a:hover {
+        text-decoration: underline;
+    }
+
+    .hero {
+        text-align: left;
+        padding-top: 50px;
+    }
+
+    .hero h1 {
+        font-size: 36px;
+        font-weight: bold;
+        margin: 0;
+    }
+
+    .hero p {
+        font-size: 16px;
+        margin-top: 10px;
+    }
+
+    .download {
+        margin-top: 50px;
+        display: flex;
+        gap: 10px;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .download span{
+        font-size: 18px;
+        text-align: center;
+    }
+
+    .download h6 {
+        text-align: center;
+    }
+
+    .content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        gap: 30px;
+    }
+
+    .button {
+        background-color: #191A32;
+        color: white;
+    }
+    .register {
+        height: 520px;
+        width: 550px;
+        background-color: #2D2F4A;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        transition: all .2s ease-in-out;
+        border-radius: 25px;
+    }
+
+    .login {
+        height: 350px;
+        width: 450px;
+        background-color: #2D2F4A;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        transition: all .2s ease-in-out;
+        border-radius: 25px;
+    }
+    .item {
+        height: 500px;
+        width: 350px;
+        background-color: #2D2F4A;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        transition: all .2s ease-in-out;
+        border-radius: 10px;
+    }
+
+    .item:hover {
+        transform: scale(1.1);
+        border: 1px solid #FFFFFF;
+    }
+
+    .item .item-text h2 {
+        font-size: 30px;
+        margin: 0 0 10px 0;
+        font-weight: bold;
+    }
+
+    .item .item-text p {
+        font-size: 16px;
+        margin: 0;
+    }
+
+    .item {
+        font-size: 24px;
+    }
+
+    .item .dot-1, .item .dot-2, .item .dot-3, .item .dot-4{
+        background-color: #FFFFFF;
+        height:10px;
+        width: 10px;
+        border-radius: 50%;
+        position: absolute;
+    }
+
+    .item .dot-1 {
+        top: 10px;
+        left: 10px;
+    }
+
+    .item .dot-2 {
+        top: 10px;
+        right: 10px;
+    }
+
+    .item .dot-3 {
+        bottom: 10px;
+        left: 10px;
+    }
+
+    .item .dot-4 {
+        bottom: 10px;
+        right: 10px;
+    }
+
+
+
+
+
     h1, h2, h3, h4, h5, h6, p {
         overflow-wrap: break-word;
         word-wrap: break-word;
